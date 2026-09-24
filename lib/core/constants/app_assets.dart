820bdac10dbc3a8d73948ed19d38228aeb03534b
TanvirHasanEmn <TanvirHasanEmn@users.noreply.github.com>
2026-09-24 14:57:10 +0600

    feat(App Constant): Implemented  app constant to define assets path

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,38 @@
+class ImagePath {
+  ImagePath._();
+  static const String _images= 'assets/images/';
+  static const String _icons = 'assets/icons/';
+
+  //*Image path
+
+  static const String onboarding_one = '${_images}onboarding1.png';
+  static const String onboarding_two = '${_images}onboarding2.png';
+  static const String onboarding_three = '${_images}onboarding3.png';
+  static const String signin_success = '${_images}signin_success.png';
+  static const String password_change_success = '${_images}password_change_success.png';
+  static const String location_access = '${_images}location_access.png';
+  static const String avatar = '${_images}avatar.png';
+  static const String plumber = '${_images}plumber.png';
+  static const String review = '${_images}review.png';
+
+
+
+
+
+
+  //*icons path
+  static const String splash = '${_icons}splash.png';
+  static const String google = '${_icons}google.svg';
+  static const String role = '${_icons}role.png';
+  static const String profile = '${_icons}profile.png';
+  static const String pin = '${_icons}pin.png';
+  static const String bell = '${_icons}bell.png';
+  static const String edit_profile = '${_icons}editprofile.png';
+  static const String image_upload = '${_icons}image_upload.png';
+  static const String doc_upload = '${_icons}doc_upload.png';
+  static const String filter = '${_icons}filter.png';
+  static const String filter2 = '${_icons}Filter2.png';
+  static const String close = '${_icons}close.png';
+  static const String mappin = '${_icons}close.png';
+  static const String defaultAvatar = '${_icons}default_avatar.jpg';
+}
---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,7 @@
+class AppConstants {
+  AppConstants._();
+
+  static const double defaultPadding = 16.0;
+  static const Duration defaultTimeout = Duration(seconds: 30);
+  static const int paginationLimit = 20;
+}
